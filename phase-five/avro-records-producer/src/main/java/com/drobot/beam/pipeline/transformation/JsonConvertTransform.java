017556422422afddb395f63cd1e87f9aac0f27be
Uladzislau Drobat <uladzislau_drobat@epam.com>
2021-09-17 13:54:34 +0300

    Fixed serialization

--- phase-five/avro-records-producer/src/main/java/com/drobot/beam/pipeline/transformation/JsonConvertTransform.java
+++ phase-five/avro-records-producer/src/main/java/com/drobot/beam/pipeline/transformation/JsonConvertTransform.java
@@ -1,11 +1,19 @@
 package com.drobot.beam.pipeline.transformation;
 
-import org.apache.avro.generic.GenericData;
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericDatumWriter;
 import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.io.Encoder;
+import org.apache.avro.io.EncoderFactory;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.PTransform;
 import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.values.PCollection;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class JsonConvertTransform extends PTransform<PCollection<GenericRecord>, PCollection<String>> {
 
@@ -13,13 +21,22 @@ public class JsonConvertTransform extends PTransform<PCollection<GenericRecord>,
 
         @SuppressWarnings("unused")
         @ProcessElement
-        public void processElement(@Element GenericRecord record, OutputReceiver<String> outputReceiver) {
+        public void processElement(@Element GenericRecord record, OutputReceiver<String> outputReceiver)
+                throws IOException{
             String result = encode(record);
             outputReceiver.output(result);
         }
 
-        private String encode(GenericRecord record) {
-            return GenericData.get().toString(record);
+        private String encode(GenericRecord record) throws IOException {
+            Schema schema = record.getSchema();
+            try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+                DatumWriter<GenericRecord> writer = new GenericDatumWriter<>(schema);
+                Encoder encoder = EncoderFactory.get().jsonEncoder(schema, outputStream, false);
+                writer.write(record, encoder);
+                outputStream.flush();
+                encoder.flush();
+                return new String(outputStream.toByteArray(), StandardCharsets.UTF_8);
+            }
         }
     }
 
